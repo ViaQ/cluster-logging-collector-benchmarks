d2368da064e49e7e83eead123ff2b5f5100e186f
Eran Raichstein <eran.raichstein@gmail.com>
2021-04-04 13:43:30 +0300

    fix small but in test and add go folder to git

---
+++ go/check-logs-sequence/check-logs-sequence.go
@@ -0,0 +1,294 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/papertrail/go-tail/follower"
+	log "github.com/sirupsen/logrus"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type logSourceInfo struct {
+	loggedCount    int64
+	collectedCount int64
+	smallestSeq    int64
+	biggestSeq     int64
+	lastCollected  int64
+	hashID         string
+}
+
+type reportStatistics struct {
+	totalLogsCollectedCount int64
+	startMonitoringTime     time.Time
+}
+
+func main() {
+
+	var fluentLogFileName string
+	var reportCount int64
+	var logLevel string
+	var seekFrom int
+
+	flag.StringVar(&fluentLogFileName, "f", "0.log", "fluent log file to tail")
+	flag.Int64Var(&reportCount, "c", 100, "number of logs between reports")
+	flag.StringVar(&logLevel, "l", "fatal", "Logging level e.g debug, info")
+	flag.IntVar(&seekFrom, "s", io.SeekEnd, "Tail seek from: 2 - end , 1 - current, 0 - start")
+	flag.Parse()
+
+	log.SetFormatter(&log.TextFormatter{})
+	log.SetOutput(os.Stdout)
+	lvl, err := log.ParseLevel(logLevel)
+	if err != nil {
+		lvl = log.FatalLevel
+	}
+	log.SetLevel(lvl)
+
+	logsCurrentInfo := make(map[string]logSourceInfo)
+	logsTotalInfo := make(map[string]logSourceInfo)
+	reportData := reportStatistics{}
+
+	reportData.startMonitoringTime = time.Now().Add(-1* time.Second)
+
+	// forever loop tailing on the file
+	for {
+		log.Info("Watching file: ", fluentLogFileName)
+		time.Sleep(1 * time.Millisecond)
+
+		t, err := follower.New(fluentLogFileName, follower.Config{
+			Whence: seekFrom,
+			Offset: 0,
+			Reopen: true,
+		})
+
+		if err != nil {
+			log.Error("follower.New: Error ", err)
+			continue
+		}
+
+		for line := range t.Lines() {
+			errParse, name, seq, logTag, hashID := parseLine(line.String())
+			if errParse != nil {
+				log.Error("Error in Line: ", line.String())
+				log.Error(errParse)
+				continue
+			}
+
+			// Skip not full log lines
+			if logTag != "F" {
+				log.Error("Skipping line with logTag not [F]: ", line.String())
+				continue
+			}
+
+			if _, ok := logsCurrentInfo[name]; !ok {
+				logsCurrentInfo[name] = logSourceInfo{hashID: hashID, smallestSeq: seq, biggestSeq: seq}
+			}
+
+			if _, ok := logsTotalInfo[name]; !ok {
+				logsTotalInfo[name] = logSourceInfo{hashID: hashID, smallestSeq: seq, biggestSeq: seq}
+			}
+
+			// Get current entry information
+			entry := logsCurrentInfo[name]
+			if entry.hashID != hashID {
+				fmt.Printf("Error in Line: %s\n", line.String())
+				fmt.Printf("Source Identification Hash ID (current) is wrong for [%s]: should be %s but is %s\n",name, entry.hashID, hashID )
+				continue
+			}
+
+			// Get total entry information
+			totalEntry := logsTotalInfo[name]
+			if totalEntry.hashID != hashID {
+				fmt.Printf("Error in Line: %s\n", line.String())
+				fmt.Printf("Source Identification Hash ID (total) is wrong for [%s]: should be %s but is %s\n",name, totalEntry.hashID, hashID )
+				continue
+			}
+
+			// calculate current metrics
+			if entry.lastCollected != 0 && seq < entry.lastCollected {
+				log.Error("Error in Line: ", line.String())
+				log.Errorf("Out of order sequence for[%s]: last collected seq is %d but we got %d\n", name, entry.lastCollected, seq )
+			}
+			if seq < entry.smallestSeq {
+				entry.smallestSeq = seq
+			}
+
+			if seq > entry.biggestSeq {
+				entry.biggestSeq = seq
+			}
+
+			entry.collectedCount += 1
+			entry.lastCollected = seq
+			entry.loggedCount = entry.biggestSeq - entry.smallestSeq + 1
+
+			// calculate total metrics
+			if seq < totalEntry.smallestSeq {
+				totalEntry.smallestSeq = seq
+			}
+			if seq > totalEntry.biggestSeq {
+				totalEntry.biggestSeq = seq
+			}
+
+			totalEntry.collectedCount += 1
+			totalEntry.loggedCount = totalEntry.biggestSeq - totalEntry.smallestSeq + 1
+
+			// calculate global metrics
+			reportData.totalLogsCollectedCount += 1
+
+			// persist
+			logsCurrentInfo[name] = entry
+			logsTotalInfo[name] = totalEntry
+
+			if reportData.totalLogsCollectedCount%reportCount == 0 {
+				report(reportData, logsCurrentInfo, logsTotalInfo)
+
+				// reset counting
+				logsCurrentInfo = make(map[string]logSourceInfo)
+			}
+		}
+	}
+}
+func parseLine(line string) (err error, name string, seq int64, logTag string, hashID string) {
+	// Parse logTag
+	logTagStartIndex := strings.Index(line, "\"logtag\":\"")
+	if logTagStartIndex > 0 {
+		logTagStartIndex += len("\"logtag\":\"")
+		logTag = line[logTagStartIndex : logTagStartIndex+1]
+	} else {
+		logTag = "F" // assuming that if we do not have log tag it is a full line
+	}
+
+	// parse name (from path)
+	pathStartIndex := strings.Index(line, "\"path\":\"")
+	if pathStartIndex == -1 {
+		err = errors.New("parseLine: cant find path start")
+		return err, "", 0,"", ""
+	}
+	pathStartIndex += len("\"path\":\"")
+
+	pathEndIndex := strings.Index(line[pathStartIndex:], "\"")
+	if pathEndIndex == -1 {
+		err = errors.New("parseLine: cant find path end")
+		return err, "", 0,"", ""
+	}
+
+	path := line[pathStartIndex : pathStartIndex+pathEndIndex]
+
+	//// get container name from path
+	nameSliced := strings.Split(path, "_")
+	if len(nameSliced) < 1 {
+		err = errors.New("parseLine: can't parse _ in path")
+		return err, "", 0,"", ""
+	}
+	nameSliced = strings.Split(fmt.Sprintf("%s", nameSliced[0]), "/")
+	if len(nameSliced) < 5 {
+		err = errors.New("parseLine: can't parse / in path")
+		return err, "", 0,"", ""
+	}
+
+	if nameSliced[3] != "containers" {
+		err = errors.New("parseLine: can't parse / path -> follow only  /var/log/containers ")
+		return err, "", 0,"", ""
+	}
+	name = nameSliced[4]
+
+	// parse sequence and hashID (from message)
+	messageStartIndex := strings.Index(line, "\"message\":\"")
+	if messageStartIndex == -1 {
+		err = errors.New("parseLine: cant find message start")
+		return err, "", 0,"", ""
+	}
+	messageStartIndex += len("\"message\":\"")
+
+	messageEndIndex := strings.Index(line[messageStartIndex:], "\"")
+	if messageEndIndex == -1 {
+		err = errors.New("parseLine: cant find message end")
+		return err, "", 0,"", ""
+	}
+	message := line[messageStartIndex : messageStartIndex+messageEndIndex]
+
+	// get the sequence number of the log
+	logSliced := strings.Split(message, "-")
+	if len(logSliced) < 3 {
+		err = errors.New("parseLine: can't parse - in log")
+		return err, "", 0,"", ""
+	}
+	seqStr := strings.TrimSpace(logSliced[2])
+	seq, err = strconv.ParseInt(seqStr, 10, 0)
+	if err != nil {
+		err = errors.New("parseLine: can't parse ParseInt in log")
+		return err, "", 0,"", ""
+	}
+
+	// get the hashID from the log
+	hashID = strings.TrimSpace(logSliced[1])
+	return nil, name, seq, logTag, hashID
+}
+
+func report(reportData reportStatistics, logsCurrentInfo map[string]logSourceInfo, logsTotalInfo map[string]logSourceInfo) {
+	now := time.Now()
+	deltaTimeInSeconds := now.Unix() - reportData.startMonitoringTime.Unix()
+	fmt.Printf("Report at: %s\n", now.String())
+	fmt.Printf("-==-=-=-=-=\n")
+	if !reportData.startMonitoringTime.IsZero() {
+		fmt.Printf("Total number of collected logs: %d\n", reportData.totalLogsCollectedCount)
+		fmt.Printf("Total collected logs per sec: %d\n", reportData.totalLogsCollectedCount/deltaTimeInSeconds)
+		fmt.Printf("Time from start monitoring (in secs): %d\n", deltaTimeInSeconds)
+	}
+
+	fmt.Printf("-==-=-=-=-=\n")
+	tableFormat := "| %-36v | %-9v | %-9v | %-9v | %-9v | %-9v | %-9v | %-9v | %-9v |\n"
+	tableFormatLen := len(fmt.Sprintf(tableFormat, 0, 0, 0, 0, 0, 0, 0, 0, 0)) - 1
+	fmt.Printf(strings.Repeat("-", tableFormatLen) + "\n")
+	fmt.Printf(tableFormat,
+		"",
+		"Current",
+		"Lines",
+		"",
+		"Total",
+		"Lines",
+		"",
+		"",
+		"")
+	fmt.Printf(strings.Repeat("-", tableFormatLen)+ "\n")
+	fmt.Printf(tableFormat,
+		"Container name",
+		"Logged",
+		"Collected",
+		"Loss",
+		"Logged",
+		"Lo./Sec",
+		"Collected",
+		"Co./Sec",
+		"Loss",
+	)
+	fmt.Printf(strings.Repeat("-", tableFormatLen)+ "\n")
+
+	names := make([]string, 0)
+	for k := range logsTotalInfo {
+		names = append(names, k)
+	}
+	sort.Strings(names)
+
+	for _, name := range names {
+		entry := logsCurrentInfo[name]
+		totalEntry := logsTotalInfo[name]
+		fmt.Printf(tableFormat, name,
+			entry.loggedCount,
+			entry.collectedCount,
+			entry.loggedCount-entry.collectedCount,
+			totalEntry.loggedCount,
+			totalEntry.loggedCount/deltaTimeInSeconds,
+			totalEntry.collectedCount,
+			totalEntry.collectedCount/deltaTimeInSeconds,
+			totalEntry.loggedCount-totalEntry.collectedCount,
+		)
+
+	}
+	fmt.Printf("\n\n")
+}
---
+++ go/log-stressor/log-stressor.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+    "fmt"
+    "log"
+    "math/rand"
+    "strings"
+    "time"
+)
+
+const  minBurstMessageCount = 100
+const  numberOfBursts = 10
+
+func main() {
+
+    var payloadGen string
+    var distribution string
+    var payloadSize int
+    var messagesPerSecond int
+
+    flag.StringVar(&payloadGen, "payload-gen", "fixed", "Payload generator [enum] (default = fixed)")
+    flag.StringVar(&distribution, "distribution", "fixed", "Payload distribution [enum] (default = fixed)")
+    flag.IntVar(&payloadSize, "payload_size", 100, "Payload length [int] (default = 100)")
+    flag.IntVar(&messagesPerSecond, "msgpersec", 1, "Number of messages per second (default = 1)")
+
+    flag.Parse()
+
+    var rnd = rand.New( rand.NewSource(time.Now().UnixNano()))
+    hash := fmt.Sprintf("%032X", rnd.Uint64())
+
+    bursts := 1
+    if messagesPerSecond > minBurstMessageCount {
+        bursts = numberOfBursts
+    }
+
+    messageCount := 0
+    startTime := time.Now().Unix() -1
+    for {
+        payload := strings.Repeat("*", payloadSize)
+        for i := 0; i < messagesPerSecond/bursts; i++ {
+            log.Printf("goloader seq - %s - %010d - %s",hash, messageCount, payload)
+            messageCount ++
+        }
+        
+        sleep := 1.0/ float64(bursts)
+        deltaTime := int(time.Now().Unix() - startTime)
+
+        messagesLoggedPerSec :=  messageCount / deltaTime
+        if messagesLoggedPerSec >= messagesPerSecond {
+            time.Sleep(time.Duration(sleep * float64(time.Second)))
+        }
+    }
+}
